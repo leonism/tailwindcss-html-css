4ad4273f594c0b9a5cb133639bf46f6b9ead2fca
gerry leo nugroho <gerryleonugroho@gmail.com>
2022-11-01 18:53:18 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "3rem",
+    },
+    extend: {
+
+    }
+  },
+  plugins: [],
+}
+
+// content: ["./*.{html}"],
+  // content: ["./*.html"],
+  // content: ["./*.{html}"],
+  // content: ["./**/*.{html,js}"],
