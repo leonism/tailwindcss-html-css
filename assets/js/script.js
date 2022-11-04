df8e92230bea8f04ec502393569d994abf8b7bc9
gerry leo nugroho <gerryleonugroho@gmail.com>
2022-11-04 21:09:35 +0700

    easier javascript to understand here

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 const toggleNavigation = () => {
-  const navigationLinks = document.querySelector("[data-nav-links]")
-  
+  const navigationLinks = document.querySelector("[data-nav-links]");
+
   // checking the class name, if navigation link has  hidden name or not
   if (navigationLinks.classList.contains("hidden")) {
-  // if found, remove the class
+    // if found, remove the class
     navigationLinks.classList.remove("hidden");
   } else {
+    // if not, add the class
     navigationLinks.classList.add("hidden");
   }
-}
+};
