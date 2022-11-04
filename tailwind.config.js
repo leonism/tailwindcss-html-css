3ff090384c595dc3f1b14e71ebb482705ef9d4bd
gerry leo nugroho <gerryleonugroho@gmail.com>
2022-11-04 17:21:12 +0700

    adding hamburger

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,13 @@ module.exports = {
     },
     screens: {
       sm: '480px',
+      // @media (min-width: 480px) { ... }
       md: '768px',
+      // @media (min-width: 768px) { ... }
       lg: '976px',
+      // @media (min-width: 976px) { ... }
       xl: '1440px',
+      // @media (min-width: 1440px) { ... }
     },
     extend: {
 
